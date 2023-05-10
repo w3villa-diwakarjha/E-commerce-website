863d06fa1abb8d171de3b45670de5ed6e2530438
Diwakar Jha <diwakar.jha@w3villa.com>
2023-05-10 12:52:19 +0530

    updated loin

--- Assets/Js/login.js
+++ Assets/Js/login.js
@@ -34,25 +34,8 @@ btnlogin1.addEventListener('click', showModal1)
 var closedd = document.querySelector('.cutt')
 closedd.addEventListener('click', closeModal1)
 
-
-// let k=document.getElementById('username')
-// console.log(k)
-
-// let p=document.getElementById('password')
-// console.log(p)
-
-// let l=document.getElementById('email')
-// console.log(l)
-
-// let d=document.getElementById('pass')
-// console.log(d)
-
-// let c=document.getElementById('cpass')
-// console.log(c)
-
 function signup(e){
     var userinfo= JSON.parse(localStorage.getItem("username")) || []
-    console.log(userinfo)
     event.preventDefault();
     let email=document.getElementById('email').value;
     let password=document.getElementById('pass').value;
@@ -63,7 +46,6 @@ function signup(e){
         alert('Password and Confirm Password Should be Match');
     }else if(email.length!=0){
         user_data=JSON.parse(localStorage.getItem('username')) || []
-        console.log(user_data)
         var k=0;
         user_data.forEach(element => {
             if(element.email === email){
@@ -83,7 +65,6 @@ function signup(e){
             userinfo.push(user)
             let json=JSON.stringify(userinfo);
             localStorage.setItem("username",json)
-            console.log("user added")
         }
     }
     
@@ -92,21 +73,15 @@ function signup(e){
 function login(e){
     event.preventDefault();
     let email1=document.getElementById('username').value;
-    console.log(email1)
     let password1=document.getElementById('password').value;
-    console.log(password1)
     let users= JSON.parse(localStorage.getItem('username'))
-    console.log(users)
     var m=0;
     users.forEach(element=>{
-        console.log(element.email)
-        console.log(element.Password)
         if((element.email==email1)&&(element.Password==password1)){
             m=1;
         }
     });
     if(m==1){
-        alert('login')
         window.location = "https://w3villa-diwakarjha.github.io/E-commerce-website/ "
     }else{
         alert('Please Enter Valid Email or Password')
