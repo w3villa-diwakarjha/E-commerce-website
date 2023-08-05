01113c616d10c77ff670e6ace99038a6d4c90527
Diwakar Jha <diwakar.jha@w3villa.com>
2023-08-05 15:14:22 +0530

    Implement Login Signup

--- Assets/Js/login.js
+++ Assets/Js/login.js
@@ -1,95 +1,86 @@
-// ########################## Login ################################
-function showModal() {
-    document.querySelector('.overlay').classList.add('showoverlay');
-    document.querySelector('.loginform').classList.add('showloginform');
-}
-
-function closeModal() {
-    document.querySelector('.overlay').classList.remove('showoverlay');
-    document.querySelector('.loginform').classList.remove('showloginform');
-}
-
-var btnlogin = document.querySelector('.login-persons')
-btnlogin.addEventListener('click', showModal)
 
 
-var close = document.querySelector('.cut')
-console.log(close)
-close.addEventListener('click', closeModal)
-
-// ######################## Register #############################
-function showModal1() {
-    document.querySelector('.overlay1').classList.add('showoverlay');
-    document.querySelector('.loginform1').classList.add('showloginform');
-}
-
-function closeModal1() {
-    document.querySelector('.overlay1').classList.remove('showoverlay');
-    document.querySelector('.loginform1').classList.remove('showloginform');
-}
-
-var btnlogin1 = document.querySelector('.register-mains')
-btnlogin1.addEventListener('click', showModal1)
-
-
-var closedd = document.querySelector('.cutt')
-closedd.addEventListener('click', closeModal1)
-
-function signup(e){
-    var userinfo= JSON.parse(localStorage.getItem("username")) || []
+function signup(e) {
+    var userinfo = JSON.parse(localStorage.getItem("login")) || []
     event.preventDefault();
-    let email=document.getElementById('email').value;
-    let password=document.getElementById('pass').value;
-    let cpassword=document.getElementById('cpass').value;
-    if(email.length==0||cpassword.length==0||password.length==0){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('pass').value;
+    let cpassword = document.getElementById('cpass').value;
+    if (email.length == 0 || cpassword.length == 0 || password.length == 0) {
         alert('All Field are Required')
-    }else if(password!=cpassword){
+    } else if (password != cpassword) {
         alert('Password and Confirm Password Should be Match');
-    }else if(email.length!=0){
-        user_data=JSON.parse(localStorage.getItem('username')) || []
-        var k=0;
+    } else if (email.length != 0) {
+        user_data = JSON.parse(localStorage.getItem('login')) || []
+        var k = 0;
         user_data.forEach(element => {
-            if(element.email === email){
-                k=1;
+            if (element.email === email) {
+                k = 1;
 
             }
         });
-        if(k==1){
+        if (k == 1) {
             alert('Already Exist')
-        }else if((email.length!=0||cpassword.length!=0||password.length!=0)||(password===cpassword)){
+        } else if ((email.length != 0 || cpassword.length != 0 || password.length != 0) || (password === cpassword)) {
 
-            var user={
+            var user = {
                 email: email,
-                Password:password,
-                CPassword:cpassword
+                Password: password,
+                CPassword: cpassword
             }
             userinfo.push(user)
-            let json=JSON.stringify(userinfo);
-            localStorage.setItem("username",json)
+            let json = JSON.stringify(userinfo);
+            localStorage.setItem("login", json);
         }
     }
-    
+
 }
 
-function login(e){
+function login(e) {
     event.preventDefault();
-    let email1=document.getElementById('username').value;
-    let password1=document.getElementById('password').value;
-    let users= JSON.parse(localStorage.getItem('username'))
-    var m=0;
-    users.forEach(element=>{
-        if((element.email==email1)&&(element.Password==password1)){
-            m=1;
+    let email1 = document.getElementById('username').value;
+    let password1 = document.getElementById('password').value;
+    let users = JSON.parse(localStorage.getItem('login'))
+    var m = 0;
+    users.forEach(element => {
+        if ((element.email == email1) && (element.Password == password1)) {
+            m = 1;
         }
     });
-    if(m==1){
-        window.location = "https://w3villa-diwakarjha.github.io/E-commerce-website/ "
-    }else{
+    if (m == 1) {
+        // window.location = "https://w3villa-diwakarjha.github.io/E-commerce-website/ "
+        localStorage.setItem('loginStatus', 'true');
+        const loginregister = document.getElementById('loginstatus').style.display = "none";
+        document.getElementById('logoutstatus').style.display="flex";
+
+        console.log(loginregister)
+    } else {
         alert('Please Enter Valid Email or Password')
     }
 }
 
 
+function logout(){
+    localStorage.setItem('loginStatus', false);
+    const logoutbutton=document.getElementById('logoutstatus').style.display="none";
+    document.getElementById('loginstatus').style.display = "flex";
+
+    console.log(logoutbutton)
+    
+}
+
+const loginStatus= localStorage.getItem('loginStatus')
+if(loginStatus==='true')
+{
+    document.getElementById('loginstatus').style.display = "none";
+    document.getElementById('logoutstatus').style.display="flex";
+}
+else{
+    document.getElementById('logoutstatus').style.display="none";
+    document.getElementById('loginstatus').style.display = "flex";
+}
 
 
 
+
+console.log("Hi")
---
+++ Assets/Js/modal.js
@@ -0,0 +1,35 @@
+// ########################## Login ################################
+function showModal() {
+    document.querySelector('.overlay').classList.add('showoverlay');
+    document.querySelector('.loginform').classList.add('showloginform');
+}
+
+function closeModal() {
+    document.querySelector('.overlay').classList.remove('showoverlay');
+    document.querySelector('.loginform').classList.remove('showloginform');
+}
+
+var btnlogin = document.querySelector('.login-persons')
+btnlogin.addEventListener('click', showModal)
+
+
+var close = document.querySelector('.cut')
+close.addEventListener('click', closeModal)
+
+// ######################## Register #############################
+function showModal1() {
+    document.querySelector('.overlay1').classList.add('showoverlay');
+    document.querySelector('.loginform1').classList.add('showloginform');
+}
+
+function closeModal1() {
+    document.querySelector('.overlay1').classList.remove('showoverlay');
+    document.querySelector('.loginform1').classList.remove('showloginform');
+}
+
+var btnlogin1 = document.querySelector('.register-mains')
+btnlogin1.addEventListener('click', showModal1)
+
+
+var closedd = document.querySelector('.cutt')
+closedd.addEventListener('click', closeModal1)
